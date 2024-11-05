a0067d6bfcd93a04401710665fc5460f19925423
LMGOT3 <joziahwalters70@gmail.com>
2024-11-05 12:16:41 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,19 +14,19 @@
         console.error('An error occured. Null Json', error)
         return null;
     }
-}
 
 function createCharacterCard ({id, gender, image, name, ki, maxKi, race, affiliation}){
     return `
         <div class="card m-4" style="width: 30rem">
-            <img src="${image}" class="card-img-top" alt="characters stats ${ki} ${maxKi}.">
+            <img src="${image}" class="card-img-top" alt="${name}.">
+            <div class="col-d-8">
             <div class="card-body">
-                <div class="d-flex justify-content-between">
-                    <h5 class="card-title">${name}</h5>
-                    <div class="d-flex"> <p>${gender}</p>
-                        <i class="bi bi-star-fill me-2"></i>
-                        <p>${affiliation}</p>
-                    </div>
+                 <h5 class="card-title">${name}</h5>
+                 <h6 class="card-title">${ki}, ${maxKi}</h6>
+                 <p class="card-text"><small class="text-body-secondary">${race}<small></p>
+                 <p class="card-text"><small class="text-body-secondary">${gender}<small></p>
+                 <p class="card-text"><small class="text-body-secondary">${affiliation}<small></p>
+                 </div>
                 </div>
                 <p class="card-text mb-4">${race}</p>
                 <h3>${id} €</h3>
