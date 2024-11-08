61523e9d5ea6c15b3da28a587ac1bba6832372f9
Jaz356 <joziahwalters70@gmail.com>
2024-11-08 08:22:02 +0100

    DragonBallApi1

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
-   async function fetchCharactersJson(requestURL) {
-   const characters = document.getElementById (`characters`)
-   const Characters = await fetchApiJson(requestCharacterUrl)
-}
-    
+   const characterURL ="https://dragonball-api.com/api/characters?limit=58";
+   async function fetchCharactersJson (requestURL) {
+     
     try{
         const response = await fetch(requestURL);
         if (!response.ok){
@@ -14,50 +12,83 @@
         console.error('An error occured. Null Json', error)
         return null;
     }
-
+}
 function createCharacterCard ({id, gender, image, name, ki, maxKi, race, affiliation}){
     return `
-        <div class="card m-4" style="width: 30rem">
-            <img src="${image}" class="card-img-top" alt="${name}.">
-            <div class="col-d-8">
-            <div class="card-body">
-                 <h5 class="card-title">${name}</h5>
-                 <h6 class="card-title">${ki}, ${maxKi}</h6>
-                 <p class="card-text"><small class="text-body-secondary">${race}<small></p>
-                 <p class="card-text"><small class="text-body-secondary">${gender}<small></p>
-                 <p class="card-text"><small class="text-body-secondary">${affiliation}<small></p>
-                 </div>
-                </div>
-                <p class="card-text mb-4">${race}</p>
-                <h3>${id} €</h3>
-            </div>
+        <div class="card" style="width: 18rem;">
+        <img src="${image}" class="card-img-top" alt="${name}">
+        <div class="card-body">
+            <h5 class="card-title">"${name}"</h5>
+            <p class="card-text">"${ki}"</p>
+             <p class="card-text">"${maxKi}"</p>
+              <p class="card-text">"${id}"</p>
+            <p class="card-text">"${gender}"</p>
+             <p class="card-text">"${race}"</p>
+              <p class="card-text">"${affiliation}"</p>
         </div>
-    `;
+        </div>
+            `;
 }
+
 async function displayCharacters(){
-    const shopSection = document.getElementById('shopSection');
-    const CharacterData = await fetchCharactorJson();
+    const characterSection = document.getElementById('characterSection');
+    const CharacterData = await fetchCharactersJson(characterURL);
     
-    if (CharacterData && CharacterData.character){
-        const CharacterCard = CharactersData.Character.map(createCharacterCard).join('');
-        shopSection.innerHTML = CharacterCard;
+    if (CharacterData && CharacterData.items){
+        const CharacterCard = CharacterData.items.map(createCharacterCard).join('');
+        characterSection.innerHTML = CharacterCard;
     }
     else{
-        shopSection.innerHTML = `<p>Characters Json couldn't load</p>`;
+        characterSection.innerHTML = `<p>Characters Json couldn't load</p>`;
     }
 }
 
 displayCharacters();
 
 
-const header = document.querySelector("header");
-const toggleClass = "is-sticky";
 
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  if (currentScroll > 150) {
-    header.classList.add(toggleClass);
-  } else {
-    header.classList.remove(toggleClass);
-  }
-});
+
+const planetURL ="https://dragonball-api.com/api/planets?limit=20";
+async function fetchPlanetJson (requestURL) {
+  
+ try{
+     const response = await fetch(requestURL);
+     if (!response.ok){
+         throw new Error(`An error occured. Json request failed ${response.status}.`)
+     }
+     return await response.json();
+ } 
+ catch (error){
+     console.error('An error occured. Null Json', error)
+     return null;
+ }
+}
+function createPlanetCard ({id, isDestroyed, image, name, description}){
+ return `
+     <div class="card" style="width: 18rem;">
+     <img src="${image}" class="card-img-top" alt="${name}">
+     <div class="card-body">
+         <h5 class="card-title">"${name}"</h5>
+         <p class="card-text">"${isDestroyed}"</p>
+          <p class="card-text">"${description}"</p>
+           <p class="card-text">"${id}"</p>
+     </div>
+     </div>
+         `;
+}
+
+async function displayPlanets(){
+ const planetSection = document.getElementById('planetSection');
+ const planetData = await fetchPlanetJson(planetURL);
+ 
+ if (planetData && planetData.items){
+     const planetsCard = planetData.items.map(createPlanetCard).join('');
+     planetSection.innerHTML = planetsCard;
+ }
+ else{
+     planetSection.innerHTML = `<p>Characters Json couldn't load</p>`;
+ }
+}
+
+displayPlanets();
+
